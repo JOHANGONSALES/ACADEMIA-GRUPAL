a032a83fbffdd3878e6951007607a0b444e56185
JOHAN <jagg.gonsales@gmail.com>
2023-06-09 15:46:08 +0200

    inicio

---
+++ src/Entity/Alumnos.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlumnosRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AlumnosRepository::class)]
+class Alumnos
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $primer_apellido = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $segundo_apellido = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fecha_nacimiento = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $domicilio = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $poblacion = null;
+
+    #[ORM\Column]
+    private ?int $codigo_postal = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telefono = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getPrimerApellido(): ?string
+    {
+        return $this->primer_apellido;
+    }
+
+    public function setPrimerApellido(string $primer_apellido): static
+    {
+        $this->primer_apellido = $primer_apellido;
+
+        return $this;
+    }
+
+    public function getSegundoApellido(): ?string
+    {
+        return $this->segundo_apellido;
+    }
+
+    public function setSegundoApellido(string $segundo_apellido): static
+    {
+        $this->segundo_apellido = $segundo_apellido;
+
+        return $this;
+    }
+
+    public function getFechaNacimiento(): ?\DateTimeInterface
+    {
+        return $this->fecha_nacimiento;
+    }
+
+    public function setFechaNacimiento(\DateTimeInterface $fecha_nacimiento): static
+    {
+        $this->fecha_nacimiento = $fecha_nacimiento;
+
+        return $this;
+    }
+
+    public function getDomicilio(): ?string
+    {
+        return $this->domicilio;
+    }
+
+    public function setDomicilio(string $domicilio): static
+    {
+        $this->domicilio = $domicilio;
+
+        return $this;
+    }
+
+    public function getPoblacion(): ?string
+    {
+        return $this->poblacion;
+    }
+
+    public function setPoblacion(string $poblacion): static
+    {
+        $this->poblacion = $poblacion;
+
+        return $this;
+    }
+
+    public function getCodigoPostal(): ?int
+    {
+        return $this->codigo_postal;
+    }
+
+    public function setCodigoPostal(int $codigo_postal): static
+    {
+        $this->codigo_postal = $codigo_postal;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(string $telefono): static
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
