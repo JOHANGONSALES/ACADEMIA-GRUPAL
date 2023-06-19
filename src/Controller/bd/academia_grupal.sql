37e38b96eb4fbc2a3f7c1e6750ec48a5b40d93f7
juan carlos macias <juancmaciassalvador@gmail.com>
2023-06-19 09:41:09 +0200

    Los datos

---
+++ src/Controller/bd/academia_grupal.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO `alumnos` ( `nombre`, `primer_apellido`, `segundo_apellido`, `fecha_nacimiento`, `domicilio`, `poblacion`, `codigo_postal`, `telefono`, `email`) VALUES
+( 'Ana', 'Valverde', 'Rioja', '2007-06-06', 'Calle del barco 34', 'Villacarrillo', 33019, '123456789', 'ana@ana.com'),
+( 'José ', 'Minguez', 'Troli', '2004-06-09', 'Avenida de la Paz 43', '', 24766, '098765432', 'troli@troli.com'),
+( 'Trampantojo', 'Espinoza', 'Villa', '2003-06-10', 'Plaza de las descalzas 456', 'Madrid', 28001, '987654321', 'villa@villa.com'),
+( 'Alcantara', 'Mendez', 'Jiménez', '2004-06-17', 'Calle de los Urquijo 76', 'Andalucía', 45098, '567843290', 'alcantara.al@alcantara.com');
+
+
+
+INSERT INTO `cursos` ( `titulo`, `estado`) VALUES
+( 'JavaScript, es un lenguaje de programación interpretado, dialecto del estándar ECMAScript. Se define como orientado a objetos, ​ basado en prototipos, imperativo, débilmente tipado y dinámico.', 1),
+( 'El lenguaje de programación Java fue desarrollado originalmente por James Gosling, de Sun Microsystems (constituida en 1983 y posteriormente adquirida el 27 de enero de 2010 por la compañía Oracle),4​ y publicado en 1995 como un componente fundamental de ', 1),
+( 'CSS, en español «Hojas de estilo en cascada», es un lenguaje de diseño gráfico para definir y crear la presentación de un documento estructurado escrito en un lenguaje de marcado.​ Wikipedia', 1),
+( 'HTML, siglas en inglés de HyperText Markup Language, hace referencia al lenguaje de marcado para la elaboración de páginas web. Wikipedia', 1);
