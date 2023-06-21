91d80a18cb26b7f9da1d186b030b23f981ae7c20
juan carlos macias <juancmaciassalvador@gmail.com>
2023-06-21 10:18:30 +0200

    Cambios test

---
+++ tests/MiprimerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MiprimerTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
