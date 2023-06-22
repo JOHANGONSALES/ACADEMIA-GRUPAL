688a76d5717283a33a16a19c790d585573a6e152
juan carlos macias <juancmaciassalvador@gmail.com>
2023-06-22 05:47:36 +0200

    Algunas mejoras y test

--- src/Entity/Alumnos.php
+++ src/Entity/Alumnos.php
@@ -42,6 +42,9 @@ class Alumnos
 
     #[ORM\Column(length: 255)]
     private ?string $email = null;
+
+    #[ORM\OneToMany(mappedBy: 'fk_alumno', targetEntity: Alumnoscursos::class)]
+    private Collection $alumnoscursos;
     
     public function __toString()
     {
@@ -157,6 +160,35 @@ class Alumnos
     public function setEmail(string $email): static
     {
         $this->email = $email;
+
+        return $this;
+    }
+    /**
+     * @return Collection<int, Alumnoscursos>
+     */
+    public function getAlumnoscursos(): Collection
+    {
+        return $this->alumnoscursos;
+    }
+
+    public function addAlumnoscurso(Alumnoscursos $alumnoscurso): static
+    {
+        if (!$this->alumnoscursos->contains($alumnoscurso)) {
+            $this->alumnoscursos->add($alumnoscurso);
+            $alumnoscurso->setFkAlumno($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAlumnoscurso(Alumnoscursos $alumnoscurso): static
+    {
+        if ($this->alumnoscursos->removeElement($alumnoscurso)) {
+            // set the owning side to null (unless already changed)
+            if ($alumnoscurso->getFkAlumno() === $this) {
+                $alumnoscurso->setFkAlumno(null);
+            }
+        }
 
         return $this;
     }
